df4174b805fc918d5fac47d0f7f31edaf04569be
lioramar55 <lioramar55@gmail.com>
2022-02-11 11:18:14 +0200

    fixed small bag with the modal and menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function init() {
   document.querySelector('header button').onclick = toggleMenu
-  document.querySelector('.main-screen').onclick = toggleMenu
+  document.querySelector('body .main-screen').onclick = toggle
   document.querySelectorAll('.post-content button').forEach((btn) => (btn.onclick = toggleModal))
   document.querySelector('.btns .close').onclick = toggleModal
   document.querySelector('.btns .submit').onclick = submitEmail
@@ -13,10 +13,16 @@ function toggleMenu() {
 }
 
 function toggleModal() {
-  document.querySelector('.modal').classList.toggle('modal-open')
+  // document.querySelector('.modal').classList.toggle('modal-open')
+  document.body.classList.toggle('modal-open')
 }
 
 function submitEmail() {
   var email = document.querySelector('.modal [type=email]').value
   console.log('Hello', email)
+}
+
+function toggle() {
+  if (document.body.classList.contains('menu-open')) toggleMenu()
+  else toggleModal()
 }
