e63d01a56c247c96db17d7c4bfd0a13f5ebcdcbc
lioramar55 <lioramar55@gmail.com>
2022-02-11 11:05:52 +0200

    added modal

--- js/main.js
+++ js/main.js
@@ -3,8 +3,20 @@
 function init() {
   document.querySelector('header button').onclick = toggleMenu
   document.querySelector('.main-screen').onclick = toggleMenu
+  document.querySelectorAll('.post-content button').forEach((btn) => (btn.onclick = toggleModal))
+  document.querySelector('.btns .close').onclick = toggleModal
+  document.querySelector('.btns .submit').onclick = submitEmail
 }
 
 function toggleMenu() {
   document.body.classList.toggle('menu-open')
+}
+
+function toggleModal() {
+  document.querySelector('.modal').classList.toggle('modal-open')
+}
+
+function submitEmail() {
+  var email = document.querySelector('.modal [type=email]').value
+  console.log('Hello', email)
 }
